5df58b04d083a7446354f75b33ebe7b4f5f2bde5
sylviayoungg <101907486+sylviayoungg@users.noreply.github.com>
2023-10-08 16:57:16 -0700

    Delete form.py

--- form.py
+++
@@ -1,8 +0,0 @@
-from wtforms import (Form, validators, SubmitField, SearchField)
-
-class SearchForm(Form):
-    # textbox
-    keyword = SearchField("Keyword: ", validators=[validators.InputRequired()])
-    
-    # submit button
-    submit = SubmitField("Search")
